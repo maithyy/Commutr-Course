d1cececb5ce83f45dced8ed40670b84b21358df9
Audrey Nguyen <hoangsa9898@gmail.com>
2022-05-21 14:23:09 -0700

    final initial commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import styles from './App.module.css';
 import Hacks from "./Hacks";
 import Title from "./Title";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 // import CourseInputComponent from './components/courseInputComponent/CourseInputComponent';
 import CourseSelectionComponent from './components/CourseSelectionComponent/CourseSelectionComponent';
@@ -18,45 +18,43 @@ import ScheduleComponent from './components/ScheduleComponent/ScheduleComponent'
 function Home() {
   
   return (
-    <Router>
-      <div className={styles['Home']}>
-        <div className={styles['title']}>
-          petr planner
-        </div>
-
-        <div className={styles['description']}>
-          welcome to petr planner, the perfect planning 
-          tool for Peter the Commuter and his friends. 
-          just enter your classes and we'll help you 
-          find the best schedule for minimizing days 
-          on campus and time between classes!
-        </div>
-
-        <div className={styles['boxes']}>
-          <div className={styles['course-selection']}>
-            <CourseSelectionComponent />
-          </div>
-          <div className={styles['schedule']}>
-            <ScheduleComponent scheduleNum={1}/>
-          </div>
-        </div>
-
-        <button type="button" className={styles['hackButton']}>Commutr Hacks!</button>
-        <Link to="/">Commutr Hacks!</Link>
-
-        {/* <Switch>
-          <Route path='/Title'>
-            <Title />
-          </Route>
-
-          <Route path='/Hacks'>
-            <Hacks />
-          </Route>
-
-        </Switch> */}
-        
+    <div className={styles['Home']}>
+      <div className={styles['title']}>
+        petr planner
       </div>
-    </Router>
+
+      <div className={styles['description']}>
+        welcome to petr planner, the perfect planning 
+        tool for Peter the Commuter and his friends. 
+        just enter your classes and we'll help you 
+        find the best schedule for minimizing days 
+        on campus and time between classes!
+      </div>
+
+      <div className={styles['boxes']}>
+        <div className={styles['course-selection']}>
+          <CourseSelectionComponent />
+        </div>
+        <div className={styles['schedule']}>
+          <ScheduleComponent scheduleNum={1}/>
+        </div>
+      </div>
+
+      <button type="button" className={styles['hackButton']}>Commutr Hacks!</button>
+      {/*<Link to="/">Commutr Hacks!</Link>*/}
+
+      {/* <Switch>
+        <Route path='/Title'>
+          <Title />
+        </Route>
+
+        <Route path='/Hacks'>
+          <Hacks />
+        </Route>
+
+      </Switch> */}
+      
+    </div>
   );
 }
 
